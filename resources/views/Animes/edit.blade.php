9ab73178a60203f17a39d03892fe72fa8530f422
YwanNob <ywanobilet@gmail.com>
2025-09-25 17:01:47 +0200

    Comm ca degage

--- resources/views/Animes/edit.blade.php
+++ resources/views/Animes/edit.blade.php
@@ -99,16 +99,7 @@
                 <textarea id="commentaires" name="commentaires" rows="4" required>{{ $anime->commentaires }}</textarea>
             </div>
     <label for="rating">Ma Note :</label>
-<select name="rating" id="rating" required>
-    <option value="">-- Choisir une note --</option>
-    @for ($i = 1; $i <= 5; $i++)
-        <option value="{{ $i }}">{{ $i }}</option>
-    @endfor
-</select>
-<br><br>
-    <label for="description">Commentaire:</label>
-    <input type="text" id="description" name="description" required><br><br>
-
+    
             <button type="submit">✅ Mettre à jour</button>
         </form>
     </div>
